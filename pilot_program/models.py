7e901ec9dacc6598c9666621fdb1fa5b0192554a
jp-sypniewski <jpsypniewski@icloud.com>
2020-09-02 10:45:09 -0600

    create PilotProgram model and link to django User

--- pilot_program/models.py
+++ pilot_program/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+class PilotProgram(models.Model):
+    title = models.CharField(max_length=80)
+    quick_description = models.CharField(max_length=200)
+    full_description = models.TextField(null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title}: {self.quick_description}"
