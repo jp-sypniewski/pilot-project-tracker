4cf1116b4f52985d631930b4431f29b74c48fd9a
jp-sypniewski <jpsypniewski@icloud.com>
2020-09-02 15:57:07 -0600

    create Task model

--- pilot_program/models.py
+++ pilot_program/models.py
@@ -15,3 +15,16 @@ class PilotProgram(models.Model):
 
     def __str__(self):
         return f"{self.title}: {self.quick_description}"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=120)
+    description =  models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_at = models.DateField()
+    complete = models.BooleanField(default=False)
+    pilot_program = models.ForeignKey(PilotProgram, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return f"{self.name}"
